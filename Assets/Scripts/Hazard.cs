115f157a6fa574f02a4271770f21071a321166ef
unknown <hyangsu,shin@student.sl.on.ca>
2022-12-02 00:41:00 -0500

    Chapter 13 - Building game hazards

---
+++ Assets/Scripts/Hazard.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour
+{
+    public GameObject playerDeathPrefab;
+    public AudioClip deathClip;
+    public Sprite hitSprite;
+    private SpriteRenderer spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        // 1
+        if (coll.transform.tag == "Player") //check colliding object has player
+        {
+            // 2
+            var audioSource = GetComponent<AudioSource>();
+            if (audioSource != null && deathClip != null)
+            {
+                audioSource.PlayOneShot(deathClip);
+            }
+            // 3
+            Instantiate(playerDeathPrefab, coll.contacts[0].point,
+            Quaternion.identity);
+            spriteRenderer.sprite = hitSprite;
+            // 4
+            Destroy(coll.gameObject);
+        }
+    }
+}
