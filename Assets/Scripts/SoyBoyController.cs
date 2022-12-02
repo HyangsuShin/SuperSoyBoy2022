be6eb0ade45d7e53f5ac7f947281ae62636cac79
unknown <hyangsu,shin@student.sl.on.ca>
2022-12-02 00:51:39 -0500

    Chapter 13 - Adding sound to the game

--- Assets/Scripts/SoyBoyController.cs
+++ Assets/Scripts/SoyBoyController.cs
@@ -28,6 +28,12 @@ public class SoyBoyController : MonoBehaviour
 
     public float jump = 14f;
 
+    //Add sound
+    public AudioClip runClip;
+    public AudioClip jumpClip;
+    public AudioClip slideClip;
+    private AudioSource audioSource;
+
     void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
@@ -35,6 +41,8 @@ public class SoyBoyController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         width = GetComponent<Collider2D>().bounds.extents.x + 0.1f;
         height = GetComponent<Collider2D>().bounds.extents.y + 0.2f;
+
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Start is called before the first frame update
@@ -123,6 +131,15 @@ public class SoyBoyController : MonoBehaviour
         }
     }
 
+    void PlayAudioClip(AudioClip clip)
+    {
+        if (audioSource != null && clip != null)
+        {
+            if (!audioSource.isPlaying) audioSource.PlayOneShot(clip);
+        }
+
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -157,13 +174,18 @@ public class SoyBoyController : MonoBehaviour
         }
 
         //Ch 13
-        if (PlayerIsOnGround() && isJumping == false)
+        if (PlayerIsOnGround() && !isJumping)
         {
             if (input.y > 0f)
             {
                 isJumping = true;
+                PlayAudioClip(jumpClip);
             }
             animator.SetBool("IsOnWall", false);
+            if (input.x < 0f || input.x > 0f)
+            {
+                PlayAudioClip(runClip);
+            }
         }
 
         if (jumpDuration > jumpDurationThreshold) input.y = 0f;
@@ -203,12 +225,14 @@ public class SoyBoyController : MonoBehaviour
         //rb.velocity = new Vector2(xVelocity, rb.velocity.y);
         rb.velocity = new Vector2(xVelocity, yVelocity);    //Ch 13
 
-        if (IsWallToLeftOrRight() && !PlayerIsOnGround() && input.y == 1)
+        if (IsWallToLeftOrRight() && !PlayerIsOnGround()
+&& input.y == 1)
         {
             rb.velocity = new Vector2(-GetWallDirection() * speed
             * 0.75f, rb.velocity.y);
             animator.SetBool("IsOnWall", false);
             animator.SetBool("IsJumping", true);
+            PlayAudioClip(jumpClip);
         }
         else if (!IsWallToLeftOrRight())
         {
@@ -218,6 +242,7 @@ public class SoyBoyController : MonoBehaviour
         if (IsWallToLeftOrRight() && !PlayerIsOnGround())
         {
             animator.SetBool("IsOnWall", true);
+            PlayAudioClip(slideClip);
         }
 
         //Ch 13
